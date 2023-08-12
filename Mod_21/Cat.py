fd0d05d6109fa95b97f3faeb562f81286f7fb8eb
Zohgen <136417801+Zohgen@users.noreply.github.com>
2023-08-12 12:02:20 +0300

    Update Cat.py

--- Mod_21/Cat.py
+++ Mod_21/Cat.py
@@ -12,26 +12,3 @@ class Cat_pet_house:
 
     def getGAge(self):
         return self.age
-
-    #def init_from_dict(self, event_dict):
-     #   self.name = event_dict.get("name")
-      #  self.gender = event_dict.get("gender")
-       # self.age = event_dict.get("age")
-
-
-# cats=[
-#     {
-#         "name" : "Барон",
-#         "gender" : "Мальчик",
-#         "age" : "2 года",
-#     },
-#     {
-#         "name": "Сэм",
-#         "gender": "Мальчик",
-#         "age": "2 years",
-#     },
-# ]
-# for event in cats:
-#     event_obj = Cat_pet_house()
-#     event_obj.init_from_dict(event)
-#     print(event_obj.name,event_obj.gender,event_obj.age)
