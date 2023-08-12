058262eb4704a37a462d7b983d3016cb8b521772
Zohgen <136417801+Zohgen@users.noreply.github.com>
2023-08-12 12:02:05 +0300

    Update cat_obj.py

--- Mod_21/cat_obj.py
+++ Mod_21/cat_obj.py
@@ -1,9 +1,14 @@
 from Cat import Cat_pet_house
 
+cat1=Cat_pet_house("Барон","Мальчик","2 года")
+cat2=Cat_pet_house("Сэм","Мальчик","2 года")
 
 
-cat=Cat_pet_house(cats)
+print("cat1.name=", cat1.name)
+print("cat1.gender=", cat1.gender)
+print("cat1.age=", cat1.age)
 
 
-print (cat)
-
+print("cat2.name=", cat2.name)
+print("cat2.gender=", cat2.gender)
+print("cat2.age=", cat2.age)
