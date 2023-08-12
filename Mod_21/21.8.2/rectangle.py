f186e6b7923d6e1f5c2d7a75e1eb4937623ac759
Zohgen <136417801+Zohgen@users.noreply.github.com>
2023-08-12 12:22:56 +0300

    Add files via upload

---
+++ Mod_21/21.8.2/rectangle.py
@@ -0,0 +1,33 @@
+class Rectangle:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def getWidth(self):
+        return self.a
+
+    def getHeight(self):
+        return self.b
+
+    def getArea(self):
+        return self.a*self.b
+
+
+class Square:
+    def __init__(self,a):
+        self.a=a
+    def get_area_square(self):
+        return self.a**2
+
+
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+
+    def get_area_circle(self):
+        pi=3.14
+        return (pi*self.radius)**2
+
+
+
+
